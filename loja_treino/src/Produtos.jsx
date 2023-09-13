1971e6ba71910eabf9f2e95c00cd5bdd4309ab53
Victor Paiva dos Santos <victor.santos@ccc.ufcg.edu.br>
2023-09-13 11:47:03 -0300

    atualizações

--- loja_treino/src/Produtos.jsx
+++ loja_treino/src/Produtos.jsx
@@ -2,18 +2,18 @@
 const produtos = [{
     id: 1,
     nome: "Cadeira",
-    imagem: "../imagens/cadeira.jfif",
+    imagem: "/src/assets/imagens/cadeira.jfif",
     preco: 999.99,
     descricao: "Descrição do produto.",
     cores: ["Preto", "Branco"],
     tamanho: []
-
+    
    
 },
 {
     id: 2,
     nome: "Microfone",
-    imagem: "../imagens/microfone.jfif",
+    imagem: "/src/assets/imagens/microfone.jfif",
     preco: 99.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -22,7 +22,7 @@ const produtos = [{
 {
     id: 3,
     nome: "Computador",
-    imagem: "../imagens/pc.jfif" ,
+    imagem: "/src/assets/imagens/pc.jfif" ,
     preco: 2999.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -31,7 +31,7 @@ const produtos = [{
 {
     id: 4,
     nome: "Celular",
-    imagem: "../imagens/celular.jfif" ,
+    imagem: "/src/assets/imagens/celular.jfif" ,
     preco: 4499.99,
     descricao: "Descrição do produto.",
     cores: ["Preto", "Branco"],
@@ -40,7 +40,7 @@ const produtos = [{
 {
     id: 5,
     nome: "Fone",
-    imagem: "../imagens/fone.jfif" ,
+    imagem: "/src/assets/imagens/fone.jfif" ,
     preco: 89.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -49,7 +49,7 @@ const produtos = [{
 {
     id: 6,
     nome: "Mouse",
-    imagem: "../imagens/mouse.jfif" ,
+    imagem: "/src/assets/imagens/mouse.jfif" ,
     preco: 119.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -58,7 +58,7 @@ const produtos = [{
 {
     id: 7,
     nome: "Teclado",
-    imagem: "../imagens/teclado.jfif" ,
+    imagem: "/src/assets/imagens/teclado.jfif" ,
     preco: 239.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -67,7 +67,7 @@ const produtos = [{
 {
     id: 8,
     nome: "Processador",
-    imagem: "../imagens/processador.jfif" ,
+    imagem: "/src/assets/imagens/processador.jfif" ,
     preco: 2789.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -76,7 +76,7 @@ const produtos = [{
 {
     id: 9,
     nome: "Memoria",
-    imagem: "../imagens/memoria.jfif" ,
+    imagem: "/src/assets/imagens/memoria.jfif" ,
     preco: 279.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -85,7 +85,7 @@ const produtos = [{
 {
     id: 10,
     nome: "Placa Mãe",
-    imagem: "../imagens/placa-mae.jfif" ,
+    imagem: "/src/assets/imagens/placa-mae.jfif" ,
     preco: 799.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -94,7 +94,7 @@ const produtos = [{
 {
     id: 11,
     nome: "Placa de Vídeo",
-    imagem: "../imagens/placa-de-video.jfif" ,
+    imagem: "/src/assets/imagens/placa-de-video.jfif" ,
     preco: 4999.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -103,7 +103,7 @@ const produtos = [{
 {
     id: 12,
     nome: "Fonte",
-    imagem: "../imagens/fonte.jfif" ,
+    imagem: "/src/assets/imagens/fonte.jfif" ,
     preco: 799.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -112,7 +112,7 @@ const produtos = [{
 {
     id: 13,
     nome: "Gabinete",
-    imagem: "../imagens/gabinete.jfif" ,
+    imagem: "/src/assets/imagens/gabinete.jfif" ,
     preco: 349.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -121,7 +121,7 @@ const produtos = [{
 {
     id: 14,
     nome: "Monitor",
-    imagem: "../imagens/monitor.jfif" ,
+    imagem: "/src/assets/imagens/monitor.jfif" ,
     preco: 1499.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -130,7 +130,7 @@ const produtos = [{
 {
     id: 15,
     nome: "Water Cooler",
-    imagem: "../imagens/wc.jfif" ,
+    imagem: "/src/assets/imagens/wc.jfif" ,
     preco: 379.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -139,7 +139,7 @@ const produtos = [{
 {
     id: 16,
     nome: "Notebook",
-    imagem: "../imagens/notebook.jfif" ,
+    imagem: "/src/assets/imagens/notebook.jfif" ,
     preco: 4899.99,
     descricao: "Descrição do produto.",
     cores: [],
